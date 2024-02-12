613d33dcb523be2c1ff3529451c6abb8cc818cb6
Baraa Khilo <baraakhilo04@gmail.com>
2024-02-12 22:10:31 +0400

    Add project files.

---
+++ clsHackParser.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class clsHackParser
+{
+private:
+
+	class clsInstruciton
+	{
+
+	};
+
+	class aInstuction : public clsInstruciton
+	{
+
+	};
+
+	class cInstuction : public clsInstruciton
+	{
+
+	};
+
+public:
+
+
+
+};
